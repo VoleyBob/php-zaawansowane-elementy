15f6715e111368499ab17fc0fb6e1ada7052e0a2
VoleyBob <voleybob@gmail.com>
2018-02-18 12:49:57 +0100

    Używam apcu_add

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo phpinfo();
+declare(strict_types=1);
+
+// echo PHP_MAX_INT; // =>147483647
+// echo phpinfo();
+
+// apcu_clear_cache ();
+
+$a = apcu_add("test", array(1,2,3,4));
+if ($a) {
+    echo "OK";
+} else {
+    echo "Błąd";
+}
+
+apcu_delete("test");
+
+
+$a = apcu_add("test", array(1,2,3,4));
+if ($a) {
+    echo "OK";
+} else {
+    echo "Błąd";
+}
+
+echo PHP_EOL;
+var_export(apcu_cache_info());
