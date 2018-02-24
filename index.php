baac75a5999fc749159207364efc6798f358c66f
VoleyBob <voleybob@gmail.com>
2018-02-24 11:36:04 +0100

    curl

--- index.php
+++ index.php
@@ -1,3 +1,56 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+<?php
+// create curl resource
+$ch = curl_init();   // przygotowujemy obiekt
+
+curl_setopt($ch, CURLOPT_ACCEPT_ENCODING, 'gzip');
+
+//curl_setopt($ch, CURLOPT_TIMEOUT_MS, 3000);
+
+// set url
+curl_setopt($ch, CURLOPT_URL, "https://wp.pl");
+
+//
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+var_dump(curl_exec($ch));
+curl_close($ch);
+
+
+exit;
+
+?>
+    </body>
+</html>
+
+
+<!-- 
+//
+$output = curl_exec($ch);
+
+// close curl resource to free up system resources
+
+
+
+
+
+
+
+
+
+
+
+
+exit;
+
+/*
 <!DOCTYPE html>
 <html>
     <head>
@@ -16,11 +69,12 @@
             </select>
             <input type="submit" />
         </form>
+        -->
         <!-- <?php //var_dump($_GET); ?> -->
-                
+        <!--        
         <?php if (!empty($_GET['val'])) :?>
             <input value="<?php echo $_GET['val'];?>" disabled>
 
         <?php endif; ?>
     </body>
-</html>
+</html>*/ -->
